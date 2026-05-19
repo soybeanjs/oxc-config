6e87d05a9f1fb53216004657862ea7c5d1c5cf7f
Soybean <soybeanjs@outlook.com>
2026-05-20 03:34:41 +0800

    style(oxlint): turn off no-underscore-dangle rule

--- src/oxlint.ts
+++ src/oxlint.ts
@@ -1,21 +1,22 @@
-import type { OxlintConfig } from "oxlint";
+import type { OxlintConfig } from 'oxlint';
 
 export const lint: OxlintConfig = {
   categories: {
-    correctness: "error",
-    suspicious: "error"
+    correctness: 'error',
+    suspicious: 'error'
   },
-  plugins: ["eslint", "typescript", "unicorn", "oxc", "import", "vue"],
-  jsPlugins: ["@soybeanjs/oxc-config/import-type-order"],
+  plugins: ['eslint', 'typescript', 'unicorn', 'oxc', 'import', 'vue'],
+  jsPlugins: ['@soybeanjs/oxc-config/import-type-order'],
   rules: {
-    "import/no-unassigned-import": "off",
-    "import-type-order/type-after-value": "warn",
-    "no-unused-vars": "off",
-    "unicorn/consistent-function-scoping": "off",
-    "unicorn/no-array-reverse": "off",
-    "unicorn/no-array-sort": "off",
-    "unicorn/no-empty-file": "off",
-    "unicorn/require-module-specifiers": "off",
-    "vue/prefer-import-from-vue": "off"
+    'import/no-unassigned-import': 'off',
+    'import-type-order/type-after-value': 'warn',
+    'no-underscore-dangle': 'off',
+    'no-unused-vars': 'off',
+    'unicorn/consistent-function-scoping': 'off',
+    'unicorn/no-array-reverse': 'off',
+    'unicorn/no-array-sort': 'off',
+    'unicorn/no-empty-file': 'off',
+    'unicorn/require-module-specifiers': 'off',
+    'vue/prefer-import-from-vue': 'off'
   }
 };
